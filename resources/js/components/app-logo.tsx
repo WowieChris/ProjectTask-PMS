38e40bf6936c8478ac316c4e160cdee9e869fca2
cjduting <139733307+cjduting@users.noreply.github.com>
2026-02-23 13:36:30 +0800

    delete, sidebar-app logo update

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -18,11 +18,11 @@ import AppLogoIcon from './app-logo-icon';
 export default function IICTDLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary bg-transparent text-sidebar-primary-foreground">
+                <AppLogoIcon className="fill-current text-white dark:text-black" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
+            <div className="ml-1 grid flex-1 text-left text-sm items-center">
+                <span className="mt-1 truncate leading-tight font-semibold items-center">
                     IICTD PMS
                 </span>
             </div>
