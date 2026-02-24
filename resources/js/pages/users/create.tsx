59bb376c71c7e2b2da2eb345bce3b7250e721ae5
cjduting <139733307+cjduting@users.noreply.github.com>
2026-02-24 13:58:16 +0800

    User create update
    create form updated

--- resources/js/pages/users/create.tsx
+++ resources/js/pages/users/create.tsx
@@ -30,6 +30,7 @@ export default function UsersCreate() {
         password: '',
         role: 'user',
         designation: '',
+        employee_num: '',
     });
 
     const submit = (e: React.FormEvent) => {
@@ -46,88 +47,104 @@ export default function UsersCreate() {
                         <CardTitle>Create User</CardTitle>
                     </CardHeader>
                     <CardContent>
-                        <form onSubmit={submit} className="space-y-4" name="formCreateUser">
-                            <div>
-                                <Label htmlFor="name">Name</Label>
-                                <Input
-                                    className="w-1/2"
-                                    id="name"
-                                    name="name"
-                                    autoComplete="name"
-                                    value={data.name}
-                                    onChange={(e) => setData('name', e.target.value)}
-                                    required
-                                />
-                                {errors.name && <p className="text-red-500">{errors.name}</p>}
+                        <form onSubmit={submit} className="space-y-4 flex flex-row gap-6" name="formCreateUser">
+                            <div className="flex w-full flex-col gap-2">
+                                <div>
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input
+                                        id="name"
+                                        name="name"
+                                        autoComplete="name"
+                                        value={data.name}
+                                        onChange={(e) => setData('name', e.target.value)}
+                                        required
+                                    />
+                                    {errors.name && <p className="text-red-500">{errors.name}</p>}
+                                </div>
+                                <div>
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        name="email"
+                                        autoComplete="email"
+                                        type="email"
+                                        value={data.email}
+                                        onChange={(e) => setData('email', e.target.value)}
+                                        required
+                                    />
+                                    {errors.email && <p className="text-red-500">{errors.email}</p>}
+                                </div>
+                                <div>
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input
+                                        id="password"
+                                        name="password"
+                                        autoComplete="new-password"
+                                        type="password"
+                                        value={data.password}
+                                        onChange={(e) => setData('password', e.target.value)}
+                                        required
+                                    />
+                                    {errors.password && <p className="text-red-500">{errors.password}</p>}
+                                </div>
+                                   <div className="flex gap-2 mt-10">
+                                    <Button type="submit" disabled={processing}>
+                                        Create
+                                    </Button>
+                                    <Button variant="outline" asChild>
+                                        <Link href="/users">Cancel</Link>
+                                    </Button>
+                                </div>
                             </div>
-                            <div>
-                                <Label htmlFor="email">Email</Label>
-                                <Input
-                                    className="w-1/2"
-                                    id="email"
-                                    name="email"
-                                    autoComplete="email"
-                                    type="email"
-                                    value={data.email}
-                                    onChange={(e) => setData('email', e.target.value)}
-                                    required
-                                />
-                                {errors.email && <p className="text-red-500">{errors.email}</p>}
-                            </div>
-                            <div>
-                                <Label htmlFor="password">Password</Label>
-                                <Input
-                                    className="w-1/2"
-                                    id="password"
-                                    name="password"
-                                    autoComplete="new-password"
-                                    type="password"
-                                    value={data.password}
-                                    onChange={(e) => setData('password', e.target.value)}
-                                    required
-                                />
-                                {errors.password && <p className="text-red-500">{errors.password}</p>}
-                            </div>
-                            <div>
-                                <Label htmlFor="role">Role</Label>
-                                  <Input type="hidden" name="role" value={data.role} />
-                                 <Select value={data.role} onValueChange={(value) => setData('role', value)}>
-                                    <SelectTrigger id="role" aria-labelledby="role-label" className="w-1/3">
-                                    <SelectValue placeholder="Select role" />
-                                    </SelectTrigger>
+                            <div className="flex w-full flex-col gap-2">
+                                <div>
+                                    <Label htmlFor="employee_num">ID Number</Label>
+                                    <Input
+                                        className="w-1/2"
+                                        id="employee_num"
+                                        name="employee_num"
+                                        autoComplete="employee_num"
+                                        type="text"
+                                        value={data.employee_num}
+                                        onChange={(e) => setData('employee_num', e.target.value)}
+                                        required
+                                    />
+                                    {errors.employee_num && <p className="text-red-500">{errors.employee_num}</p>}
+                                </div>    
+                                <div>
+                                    <Label htmlFor="role">Role</Label>
+                                    <Input type="hidden" name="role" value={data.role} />
+                                    <Select value={data.role} onValueChange={(value) => setData('role', value)}>
+                                        <SelectTrigger id="role" aria-labelledby="role-label" className="w-full">
+                                        <SelectValue placeholder="Select role" />
+                                        </SelectTrigger>
 
-                                    <SelectContent>
-                                    <SelectItem value="user">User</SelectItem>
-                                    <SelectItem value="admin">Administrator</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                                {errors.role && <p className="text-red-500">{errors.role}</p>}
-                            </div>
-                            <div>
-                                    <Label htmlFor="designation">Designation</Label>
-                                    <Input type="hidden" name="designation" value={data.designation || ''} />
-                                    <Select value={data.designation} onValueChange={(value) => setData('designation', value)}>
-                                    <SelectTrigger id="designation" aria-labelledby="designation-label" className="w-1/2">
-                                    <SelectValue placeholder="Select designation" />
-                                    </SelectTrigger>
-
-                                    <SelectContent>
-                                    <SelectItem value="Technical Support Engineer">Technical Support Engineer</SelectItem>
-                                    <SelectItem value="Field Engineer">Field Engineer</SelectItem>
-                                    <SelectItem value="System Operator">System Operator</SelectItem>
-                                    <SelectItem value="Infrastructure Engineer">Infrastructure Engineer</SelectItem>
-                                    </SelectContent>
+                                        <SelectContent>
+                                        <SelectItem value="user">User</SelectItem>
+                                        <SelectItem value="admin">Administrator</SelectItem>
+                                        </SelectContent>
                                     </Select>
-                                    {errors.designation && <p className="text-red-500">{errors.designation}</p>}
-                            </div>
-                            <div className="flex gap-2">
-                                <Button type="submit" disabled={processing}>
-                                    Create
-                                </Button>
-                                <Button variant="outline" asChild>
-                                    <Link href="/users">Cancel</Link>
-                                </Button>
-                            </div>
+                                    {errors.role && <p className="text-red-500">{errors.role}</p>}
+                                    
+                                </div>
+                                <div>
+                                        <Label htmlFor="designation">Designation</Label>
+                                        <Input type="hidden" name="designation" value={data.designation || ''} />
+                                        <Select value={data.designation} onValueChange={(value) => setData('designation', value)}>
+                                        <SelectTrigger id="designation" aria-labelledby="designation-label" className="w-full">
+                                        <SelectValue placeholder="Select designation" />
+                                        </SelectTrigger>
+
+                                        <SelectContent>
+                                        <SelectItem value="Technical Support Engineer">Technical Support Engineer</SelectItem>
+                                        <SelectItem value="Field Engineer">Field Engineer</SelectItem>
+                                        <SelectItem value="System Operator">System Operator</SelectItem>
+                                        <SelectItem value="Infrastructure Engineer">Infrastructure Engineer</SelectItem>
+                                        </SelectContent>
+                                        </Select>
+                                        {errors.designation && <p className="text-red-500">{errors.designation}</p>}
+                                </div>
+                            </div>   
                         </form>
                     </CardContent>
                 </Card>
