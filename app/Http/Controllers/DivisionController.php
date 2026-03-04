9e9bab7385dcb8a5cec84ace481ff3b264d12f93
Wowie Chris <71693971+WowieChris@users.noreply.github.com>
2026-03-04 22:48:39 +0800

    fix controller

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -8,27 +8,16 @@ use Illuminate\Http\Request;
 class DivisionController extends Controller
 {
     public function store(Request $request)
-    {
-       $data = $request->validate([
-  'user_group_id' => ['required','exists:user_groups,id'],
-  'name' => ['required','string','max:255'],
-]);
+{
+    $data = $request->validate([
+        'user_group_id' => ['required', 'exists:user_groups,id'],
+        'name' => ['required', 'string', 'max:255'],
+    ]);
 
-Division::create($data);
-return back()->with('success','Division added.');
+    Division::create($data);
 
-        $exists = Division::where('area_id', $data['area_id'])
-            ->where('name', $data['name'])
-            ->exists();
-
-        if ($exists) {
-            return back()->withErrors(['name' => 'Division already exists in this Area.']);
-        }
-
-        Division::create($data);
-
-        return back()->with('success', 'Division added.');
-    }
+    return back()->with('success', 'Division added.');
+}
 
     public function destroy(Division $division)
     {
