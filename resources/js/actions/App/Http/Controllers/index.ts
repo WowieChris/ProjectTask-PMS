596ce92c092f8cd71bb99faa42c61c3485fa98f6
Wowie Chris <71693971+WowieChris@users.noreply.github.com>
2026-03-09 09:23:08 +0800

    Remove Html Page in Laravel log when notification is sent

--- resources/js/actions/App/Http/Controllers/index.ts
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -1,10 +1,10 @@
 import Auth from './Auth'
-import BrowseController from './BrowseController'
-import DistrictController from './DistrictController'
-import DivisionController from './DivisionController'
-import Settings from './Settings'
 import UserController from './UserController'
+import Settings from './Settings'
+import DivisionController from './DivisionController'
+import DistrictController from './DistrictController'
 import UserGroupController from './UserGroupController'
+import BrowseController from './BrowseController'
 const Controllers = {
     Auth: Object.assign(Auth, Auth),
 UserController: Object.assign(UserController, UserController),
