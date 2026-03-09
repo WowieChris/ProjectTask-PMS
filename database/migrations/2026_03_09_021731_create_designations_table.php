c8c5943e3fcd9fdae451c66ea671724348f3b740
cjduting <139733307+cjduting@users.noreply.github.com>
2026-03-09 15:35:47 +0800

    index picture
    add avatar in user maintenance

--- database/migrations/2026_03_09_021731_create_designations_table.php
+++ database/migrations/2026_03_09_021731_create_designations_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('designations', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
